0b71a5d1cd5b5e8ff947f3e4c39cd5ea72a69d0e
owlsoverjericho <owlsoverjericho@protonmail.com>
2023-03-02 13:19:33 +0200

    templating engine stuff

--- djangoCafe/main_page/views.py
+++ djangoCafe/main_page/views.py
@@ -6,5 +6,6 @@ from .models import Dish, DishCategory
 def main_page(req):
     categories = DishCategory.objects.filter(is_visible=True) #query everything from this table/class (e.g. it's instances)
     dishes = Dish.objects.filter(is_visible=True)
-    
-    return render(req, 'index.html')
+    specials = Dish.objects.filter(is_visible=True, is_special=True)
+
+    return render(req, 'main_page.html', context={'categories': categories, "dishes": dishes, "specials": specials})
